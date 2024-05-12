639d11325daf7a408399a20f30099a1132a2badc
JUNGSANGYUN <sdsd98987@gmail.com>
2024-05-12 12:31:14 +0900

    테스트코드 강의 Chapter2

---
+++ dayoneTest/src/test/java/com/jyujyu/dayonetest/model/StudentPassFixture.java
@@ -0,0 +1,30 @@
+package com.jyujyu.dayonetest.model;
+
+import com.jyujyu.dayonetest.MyCalculator;
+
+public class StudentPassFixture {
+
+    public static StudentPass create(StudentScore studentScore) {
+        var calculator = new MyCalculator();
+
+        return StudentPass.builder()
+                .studentName(studentScore.getStudentName())
+                .exam(studentScore.getExam())
+                .avgScore(calculator
+                        .add(studentScore.getKorScore().doubleValue())
+                        .add(studentScore.getEnglishScore().doubleValue())
+                        .add(studentScore.getMathScore().doubleValue())
+                        .divide(3.0)
+                        .getResult()
+                )
+                .build();
+    }
+
+    public static StudentPass create(String studentName, String exam){
+        return StudentPass.builder()
+                .studentName(studentName)
+                .exam(exam)
+                .avgScore(80.0)
+                .build();
+    }
+}
